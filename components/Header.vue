6b923c46bc1697579e331f53032f9a10f6f54f39
zaskianh <144655791+zaskianh@users.noreply.github.com>
2024-02-29 08:53:55 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+        <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-11">
+        <h1>perpus digital</h1>
+        <address>
+            SMKN 4 Tasikmalaya
+            JL.Depok, Sukamenak, Purbaratu
+        </address>
+      </div>
+    </div>
+</template>
+<style scoped>
+.logo{
+  width: 100%
+}
+</style>
---
+++ pages/buku/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-12">
+            <h2 class="text-center my-4">BUKU</h2>
+            <div class="my-3">
+                <input type="search" class="form-control rounded-5" placeholder="Mau baca apa hari ini?">
+            </div>
+            <div class="my-3 text-muted">menampilkan 3 dari 3</div>
+                <div class="row">
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/asset/img/cover1.jpeg" class="cover" alt="cover 1">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/asset/img/cover2.jpg" class="cover" alt="cover 2">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <img src="~/asset/img/cover3.png " class="cover"  alt="cover 3"> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+         </div>
+        </div>
+</template>
+
+<style scoped>
+.card body {
+    width: 739%;
+    height: 577%;
+    padding: 0;
+    }
+.cover {
+        width: 146%;
+        height: 219%;
+        object-fit: cover;
+        object-position: 0 30;
+}
+
+ .cover {
+        width: 146%;
+        height: 219%;
+        object-fit: cover;
+        object-position: 0 30;
+    }
+ .cover {
+        width: 146%;
+        height: 219%;
+        object-fit: cover;
+        object-position: 0 30;
+    }
